6ae6aa121c36b1363e7d0c58bf04af71518f6950
Christopher Soden <karavol27@gmail.com>
2018-08-09 12:20:48 -0500

    Removed alerts from SignInForm

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -3,20 +3,28 @@ import {Link} from 'react-router-dom';
 import {firebase} from '../firebase/firebase';
 
 export default class SignInForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: ''
+    }
+  }
+
   onSubmit = (e) => {
     e.preventDefault();
 
     const email = document.getElementById('create_form_email_field').value;
     const password = document.getElementById('create_form_password_field').value;
 
-    let errorCode, errorMessage;
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-      errorCode = error.code;
-      errorMessage = error.message;
+    firebase.auth().signInWithEmailAndPassword(email, password).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
       if (errorCode === 'auth/wrong-password') {
-        alert('Wrong password.');
+        this.setState(() => ({error: "Wrong password."}))
+        // alert('Wrong password.');
       } else {
-        alert(errorMessage);
+        this.setState(() => ({error: errorMessage}))
+        // alert(errorMessage);
       }
     })
   }
@@ -25,12 +33,13 @@ export default class SignInForm extends React.Component {
     return (
       <div>
         <form onSubmit={this.onSubmit} >
+          {this.state.error && <p className="form-error">{this.state.error}</p>}
           <input type="text" placeholder="Email" id="create_form_email_field"/>
           <br />
           <input type="text" placeholder="Password" id="create_form_password_field"/>
           <button>Log In</button>
         </form>
-        <Link to="/profile-page">Cancel</Link>
+        <Link to="/">Cancel</Link>
       </div>
     )
   }
