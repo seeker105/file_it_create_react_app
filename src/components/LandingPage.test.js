94d0edca748c3aa13e341d4085c2916648c8043e
Christopher Soden <karavol27@gmail.com>
2018-08-09 15:24:55 -0500

    Content test working. Navigation test still not working

--- src/components/LandingPage.test.js
+++ src/components/LandingPage.test.js
@@ -5,12 +5,35 @@ import {shallow, mount} from 'enzyme';
 import {render} from 'react-dom';
 import {Link, Route} from 'react-router-dom';
 import {history} from '../App';
+import {Simulate} from 'react-dom/test-utils';
 
 test('should display create account and sign in links', () => {
   const wrapper = mount(<MemoryRouter initialEntries={['/']} initialIndex={0}>
     <LandingPage />
   </MemoryRouter>);
-  console.log(wrapper.find('a').at(0).text());
   expect(wrapper.find('a').at(0).text()).toBe("Create Account");
   expect(wrapper.find('a').at(1).text()).toBe("Sign In");
 })
+
+// test('experimental tests', () => {
+//   const leftClickEvent = {
+//     defaultPrevented: false,
+//     preventDefault() { this.defaultPrevented = true },
+//     metaKey: null,
+//     altKey: null,
+//     ctrlKey: null,
+//     shiftKey: null,
+//     button: 0
+//   }
+//   const div = document.createElement('div');
+//   const wrapper2 = render(
+//     <MemoryRouter initialEntries={['/']} initialIndex={0}>
+//       <LandingPage />
+//     </MemoryRouter>, div
+//   );
+//   // This is the way they tested the react-router package. But it doesn't work here for
+//   // some reason.
+//   console.log(div.innerHTML);
+//   Simulate.click(div.querySelector('#landing-page-link-to-create-account-form'), leftClickEvent);
+//   console.log(div.innerHTML);
+// })
