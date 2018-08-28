f1122089be4552b049771d822e28bc527c311094
Christopher Soden <karavol27@gmail.com>
2018-08-28 12:11:23 -0500

    Added styles for main Header component

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -4,7 +4,7 @@ const LoadingPage = () => {
   return (
     <div>
       <h1>Loading...</h1>
-      <img src="../images/logo.svg" alt="Loading logo" height="50" width="50" />
+      {/* <img src="../images/logo.svg" alt="Loading logo" height="50" width="50" /> */}
     </div>
   )
 }
