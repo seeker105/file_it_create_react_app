daf379394e2bc5bf686b498cce55f2f10d90471f
Christopher Soden <karavol27@gmail.com>
2018-08-28 14:56:47 -0500

    Working with the text link. Not working with the icon link

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import App from './App';
 import './css/index.css';
 import './css/LandingPage.css';
 import './css/Header.css';
-import registerServiceWorker from './registerServiceWorker';
-import App from './App';
+import './css/pageHeader.css';
+import './css/DashboardPage.css';
 
 registerServiceWorker();
 
