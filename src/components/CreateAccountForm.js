15f139ea2c39e4420b566c18e65e504d984f5a5a
Christopher Soden <karavol27@gmail.com>
2018-08-29 06:37:00 -0500

    Add styles to CreateAccountForm

--- src/components/CreateAccountForm.js
+++ src/components/CreateAccountForm.js
@@ -42,15 +42,28 @@ export default class CreateAccountForm extends React.Component {
   render () {
     return (
       <div>
-        <form onSubmit={this.onSubmit} >
-          {this.state.error && <p className="form-error">{this.state.error}</p>}
-          <input type="text" placeholder="First Name" id="create_form_first_name_field" />
-          <input type="text" placeholder="Last Name" id="create_form_last_name_field" />
-          <input type="text" placeholder="Email" id="create_form_email_field"/>
-          <input type="text" placeholder="Password" id="create_form_password_field"/>
-          <button>Register</button>
-        </form>
-        <Link to="/">Cancel</Link>
+        <div className="page-header">
+          <div className="content-container">
+            <h1 className="page-header__title">Create Account</h1>
+          </div>
+        </div>
+        <div className="small-content-container">
+          <form onSubmit={this.onSubmit}>
+            <div className="form-container">
+              {this.state.error && <p className="form-error">{this.state.error}</p>}
+              <label className="label-style">First Name</label>
+              <input type="text" placeholder="First Name" id="create_form_first_name_field" className="input-style"/>
+              <label className="label-style">Last Name</label>
+              <input type="text" placeholder="Last Name" id="create_form_last_name_field" className="input-style"/>
+              <label className="label-style">Email</label>
+              <input type="text" placeholder="Email" id="create_form_email_field" className="input-style"/>
+              <label className="label-style">Password</label>
+              <input type="text" placeholder="Password" id="create_form_password_field" className="input-style"/>
+            </div>
+            <button className="button button-register">Register</button>
+          </form>
+          <Link to="/" className="button">Cancel</Link>
+        </div>
       </div>
     )
   }
