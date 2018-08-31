03ef77953f23ad7eb40c85289e1a6674f0be6024
Christopher Soden <karavol27@gmail.com>
2018-08-31 07:24:07 -0500

    Create order confirmation page

---
+++ src/components/OrderConfirmationPage.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import store from '../store/configureStore';
+import Header from './Header';
+import {getPlanPrice, getPlanDetails} from '../utilities/planData';
+
+export default class OrderConfirmationPage extends React.Component {
+  constructor (props) {
+    super(props);
+    const accountType = store.getState().accountType;
+  }
+
+
+
+  render () {
+    return (
+      <div>
+        <Header />
+        <div className="page-header">
+          <div className="content-container">
+            <h1 className="page-header__title">Thank you!</h1>
+          </div>
+        </div>
+        <div className="medium-content-container-centered-text">
+          <h1>Thank you for your order!</h1>
+          <p>Happy Filing!</p>
+          <p>Go to <Link to="/dashboard" className="button">All Files</Link></p>
+        </div>
+
+      </div>
+    )
+  }
+}
