9bcc197cbb117b49d5552602219aa297c0901d22
Christopher Soden <karavol27@gmail.com>
2018-09-01 11:42:04 -0500

    Remove duplicate code function

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,14 +1,3 @@
-
-export const loginGenerator = (firstName, lastName, email, accountType) => {
-  return {
-    type: 'LOGIN',
-    firstName,
-    lastName,
-    email,
-    accountType
-  };
-};
-
 export const logoutGenerator = () => {
   return {
     type: 'LOGOUT'
