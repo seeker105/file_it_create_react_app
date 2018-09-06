138edee865b2660f4ed9fa12360f217058f17048
Christopher Soden <karavol27@gmail.com>
2018-09-06 12:16:52 -0500

    Add student project message prior to launch

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -28,6 +28,7 @@ export default class LandingPage extends React.Component {
             className="button">
             Sign In
           </button>
+          <p>*Note: This is a student project. It's fitness for any particular purpose is not guaranteed.</p>
         </div>
       </div>
     );
