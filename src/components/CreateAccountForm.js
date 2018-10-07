ff1a11770c6e5ff906673c27cd7e158d6aff3df8
Christopher Soden <karavol27@gmail.com>
2018-10-07 14:00:25 -0500

    Successfully testing for name on CreateAccountForm

--- src/components/CreateAccountForm.js
+++ src/components/CreateAccountForm.js
@@ -9,23 +9,59 @@ export class CreateAccountForm extends React.Component {
     super(props);
 
     this.state = {
-      error: ''
+      error: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: ''
     };
   }
 
+  onFirstNameChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      firstName: e.target.value
+    })
+  };
+
+  onLastNameChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      lastName: e.target.value
+    })
+  };
+
+  onEmailChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      email: e.target.value
+    })
+  };
+
+  onPasswordChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      password: e.target.value
+    })
+  };
+
   onSubmit = (e) => {
     e.preventDefault();
-    const email = document.getElementById('create_form_email_field').value;
-    const password = document.getElementById('create_form_password_field').value;
-    const firstName = document.getElementById('create_form_first_name_field').value;
-    const lastName = document.getElementById('create_form_last_name_field').value;
+    // const firstName = document.getElementById('create_form_first_name_field').value;
+    // const email = document.getElementById('create_form_email_field').value;
+    // const password = document.getElementById('create_form_password_field').value;
+    // const lastName = document.getElementById('create_form_last_name_field').value;
+    const firstName = this.state.firstName;
+    const lastName = this.state.lastName;
+    const email = this.state.email;
+    const password = this.state.password;
     const accountType = "0";
     localStorage.setItem('firstName', firstName);
     localStorage.setItem('lastName', lastName);
     localStorage.setItem('accountType', accountType);
 
     if (!firstName || !lastName) {
-      this.setState(() => ({error: "Name is required"}))
+      this.setState(() => ({error: "First and last name are required"}))
     } else {
       createUserWithEmailAndPassword(email, password)
         .then((credential) => {
@@ -55,13 +91,41 @@ export class CreateAccountForm extends React.Component {
             <div className="form-container">
               {this.state.error && <p className="form-error">{this.state.error}</p>}
               <label className="label-style">First Name</label>
-              <input type="text" placeholder="First Name" id="create_form_first_name_field" className="input-style"/>
+              <input
+                type="text"
+                placeholder="First Name"
+                id="create_form_first_name_field"
+                className="input-style"
+                onChange={this.onFirstNameChange}
+                value={this.state.firstName}
+              />
               <label className="label-style">Last Name</label>
-              <input type="text" placeholder="Last Name" id="create_form_last_name_field" className="input-style"/>
+              <input
+                type="text"
+                placeholder="Last Name"
+                id="create_form_last_name_field"
+                className="input-style"
+                onChange={this.onLastNameChange}
+                value={this.state.lastName}
+              />
               <label className="label-style">Email</label>
-              <input type="text" placeholder="Email" id="create_form_email_field" className="input-style"/>
+              <input
+                type="text"
+                placeholder="Email"
+                id="create_form_email_field"
+                className="input-style"
+                onChange={this.onEmailChange}
+                value={this.state.email}
+              />
               <label className="label-style">Password</label>
-              <input type="text" placeholder="Password" id="create_form_password_field" className="input-style"/>
+              <input
+                type="text"
+                placeholder="Password"
+                id="create_form_password_field"
+                className="input-style"
+                onChange={this.onPasswordChange}
+                value={this.state.password}
+              />
             </div>
             <button className="button button-register">Register</button>
           </form>
